d3a66768d250c6e89f5377dee83eb4c380050996
anurag-kumar55 <anuragkumar082005@gmail.com>
2025-11-18 23:15:31 +0530

    Add files via upload

---
+++ loop_01.10.2025/3a.c
@@ -0,0 +1,17 @@
+/*  print the pattern
+1
+23
+456
+*/
+#include <stdio.h>
+int main() {
+ int i, j, num = 1;
+ for (i = 1; i <= 3; i++) {
+ for (j = 1; j <= i; j++) {
+ printf("%d ", num);
+ num++;
+ }
+ printf("\n");
+ }
+ return 0;
+}
